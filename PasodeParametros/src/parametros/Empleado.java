c0acb6775154db41d059c42d5786def388e23bd1
Baixeras1 <baixeras1@gmail.com>
2022-02-07 21:30:49 +0100

    Actualizando los métodos

--- PasodeParametros/src/parametros/Empleado.java
+++ PasodeParametros/src/parametros/Empleado.java
@@ -6,9 +6,18 @@ public class Empleado {
 	private String nombre;
 	private int edad;
 	
+	public String getDNI() {
+		return dni;
+	}
+
+	public void setDNI(String dni) {
+		this.dni = dni;
+	}
+
 	public Empleado(String dni, String nombre, int edad) {
 		this.dni = dni;
 		this.nombre = nombre;
 		this.edad = edad;
 	}
+
 }
--- PasodeParametros/src/parametros/Main.java
+++ PasodeParametros/src/parametros/Main.java
@@ -3,8 +3,38 @@ package parametros;
 public class Main {
 
 	public static void main(String[] args) {
+		int a = 12;
+		Empleado []em=new Empleado[3];
+		for (int i=0;i<em.length;i++) {
+		String dni=String.valueOf(i);
+		em[i]=new Empleado(dni,"ana"+dni,i+18);
+		}
+		int []valores = {9,3,5};
+		queHace(a,em,valores);
+		System.out.println(em[1].getDNI());
+		System.out.println(valores[2]);
+		a += 3;
+		System.out.println(""+queHace2(em,a,valores));
+		}
+		public static void queHace(int e,Empleado[] s,int []t) {
+		e = e + 2;
+		for (int i =0;i<t.length;i++) {
+		t[i]=t[i] + e + i;
+		}
+		for (int i =0;i<s.length;i++) {
+		Empleado em = s[i];
+		em.setDNI(em.getDNI()+t[i]);
+		}
+		System.out.println(s[2].getDNI());
+		}
 
-		boolean acierto = false;
-	}
-
+		public static int queHace2(Empleado a[],int b,int[]c) {
+		int suma=0;
+		b += 4;
+		for (int i =0;i<a.length;i++) {
+		int val = Integer.parseInt((a[i].getDNI()));
+		suma = suma + c[i] + val;
+		}
+		return suma;  
+		}
 }
