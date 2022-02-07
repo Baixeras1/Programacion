9e09c78ad1bccfc8c605f4b07306b0dd65670c0f
Baixeras1 <baixeras1@gmail.com>
2022-02-07 19:47:55 +0100

    Inicio del programa

---
+++ PasodeParametros/src/parametros/Empleado.java
@@ -0,0 +1,14 @@
+package parametros;
+
+public class Empleado {
+	
+	private String dni;
+	private String nombre;
+	private int edad;
+	
+	public Empleado(String dni, String nombre, int edad) {
+		this.dni = dni;
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+}
